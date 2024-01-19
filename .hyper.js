56d26614b6fbc3210fdd11a57c1f8103f40c8852
gael <xarco@gaels-MacBook-Pro.local>
2024-01-18 20:24:19 -0800

    updated for mac

--- .hyper.js
+++ .hyper.js
@@ -8,9 +8,9 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 15,
+        fontSize: 12,
         // font family with optional fallbacks
-        fontFamily: 'Hack, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
@@ -20,104 +20,108 @@ module.exports = {
         // letter spacing as a relative unit
         letterSpacing: 0,
         // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-        cursorColor: '#FFF',
+        cursorColor: 'rgba(248,28,229,0.8)',
         // terminal text color under BLOCK cursor
         cursorAccentColor: '#000',
         // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-        cursorShape: 'UNDERLINE',
+        cursorShape: 'BLOCK',
         // set to `true` (without backticks and without quotes) for blinking cursor
-        cursorBlink: true,
+        cursorBlink: false,
         // color of the text
         foregroundColor: '#fff',
         // terminal background color
         // opacity is only supported on macOS
-        backgroundColor: 'rgba(0, 0, 0, 0.62)',
+        backgroundColor: '#000',
         // terminal selection color
-        selectionColor: 'rgb(220, 220, 220)',
+        selectionColor: 'rgba(248,28,229,0.3)',
         // border color (window, tabs)
-        borderColor: 'rgba(0, 0, 0, 0.62)',
+        borderColor: '#333',
         // custom CSS to embed in the main window
         css: `
-            .header_windowHeader {
-                border: 0;
-            }
-            .tabs_nav {
-                border: 0;
-            }
-            .tabs_list {
-                border: 0;
-                padding-left: 5px;
-                padding-right: 5px;
-            }
-            .terms_terms .terms_termGroup .term_active .term-wrapper {
-                background-color: rgba(0, 0, 0, 0.62);
-            }
-            .hyper_main {
-                background-color: rgba(0, 0, 0, 0.62);
-            }
-            .xterm canvas {
-                border-radius: 12px;
-            }
-            .tab_tab {
-                border: 0;
-                background-color: rgba(0, 0, 0, 0.62);
-                margin-inline: 3px;
-                border-radius: 5px;
-                transition: background-color 0.2s ease-in-out;
-            }
-            .tab_hasActivity {
-                color: lightyellow;
-            }
-            .tab_tab.tab_active {
-                border: 0;
-                background-color: rgba(0, 0, 0, 1) !important;
-                transition: background-color 0.2s ease-in-out;
-            }
-            .splitpane_divider_vertical {
-                margin-top: 5px;
-                margin-bottom: 5px;
-            }
-            .splitpane_divider_horizontal {
-                width: auto !important;
-                margin-left: 5px;
-                margin-right: 5px;
-            }
+        .header_windowHeader {
+            border: 0;
+        }
+        .tabs_nav {
+            border: 0;
+        }
+        .tabs_list {
+            border: 0;
+            padding-left: 5px;
+            padding-right: 5px;
+        }
+        .terms_terms .terms_termGroup .term_active .term-wrapper {
+            background-color: rgba(0, 0, 0, 0.62);
+        }
+        .hyper_main {
+            background-color: rgba(0, 0, 0, 0.62);
+        }
+        .xterm canvas {
+            border-radius: 12px;
+            padding: 5px;
+        }
+        .tab_tab {
+            border: 0;
+            background-color: rgba(0, 0, 0, 0.2);
+            margin-inline: 3px;
+            border-radius: 5px;
+            transition: background-color 0.2s ease-in-out;
+        }
+        .tab_hasActivity {
+            color: lightyellow;
+        }
+        .tabs_borderShim {
+            border: 0;
+        }
+        .tab_tab.tab_active {
+            border: 0;
+            background-color: rgba(0, 0, 0, 1) !important;
+            transition: background-color 0.2s ease-in-out;
+        }
+        .splitpane_divider_vertical {
+            margin-top: 5px;
+            margin-bottom: 5px;
+        }
+        .splitpane_divider_horizontal {
+            width: auto !important;
+            margin-left: 5px;
+            margin-right: 5px;
+        }
         `,
         // custom CSS to embed in the terminal window
-        termCSS: ``,
+        termCSS: '',
         // set custom startup directory (must be an absolute path)
         workingDirectory: '',
         // if you're using a Linux setup which show native menus, set to false
         // default: `true` on Linux, `true` on Windows, ignored on macOS
-        showHamburgerMenu: true,
+        showHamburgerMenu: '',
         // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
         // additionally, set to `'left'` if you want them on the left, like in Ubuntu
         // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-        showWindowControls: true,
+        showWindowControls: '',
         // custom padding (CSS format, i.e.: `top right bottom left`)
-        padding: '12px 12px',
+        padding: '10px 10px',
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
         colors: {
-            black: "#4C4C4C",
-            red: "#FF7F7F",
-            green: "#7FFF7F",
-            yellow: "#FFFF99",
-            blue: "#7FAAFF",
-            magenta: "#FFAFFF",
-            cyan: "#A4FFF9",
-            white: "#FFFFFC",
-            lightBlack: "#808080",
-            lightRed: "#FFB2B0",
-            lightGreen: "#AFFFAB",
-            lightYellow: "#FFFFCC",
-            lightBlue: "#B0C9FF",
-            lightMagenta: "#FFD4FF",
-            lightCyan: "#B3FFFD",
-            lightWhite: "#FFFFFF",
-            limeGreen: "#90EE90",
-            lightCoral: "#FFA07A"
+            black: '#000000',
+            red: '#C51E14',
+            green: '#1DC121',
+            yellow: '#C7C329',
+            blue: '#0A2FC4',
+            magenta: '#C839C5',
+            cyan: '#20C5C6',
+            white: '#C7C7C7',
+            lightBlack: '#686868',
+            lightRed: '#FD6F6B',
+            lightGreen: '#67F86F',
+            lightYellow: '#FFFA72',
+            lightBlue: '#6A76FB',
+            lightMagenta: '#FD7CFC',
+            lightCyan: '#68FDFE',
+            lightWhite: '#FFFFFF',
+            limeGreen: '#32CD32',
+            lightCoral: '#F08080',
         },
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
         // if left empty, your system's login shell will be used by default
@@ -146,7 +150,7 @@ module.exports = {
         // Supported Options:
         //  1. 'SOUND' -> Enables the bell as a sound
         //  2. false: turns off the bell
-        bell: false,
+        bell: 'SOUND',
         // An absolute file path to a sound file on the machine.
         // bellSoundURL: '/path/to/sound/file',
         // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
@@ -188,7 +192,7 @@ module.exports = {
         'hyperterm-dibdabs',
         'hyperterm-tabs',
         'hyper-search',
-        "hyper-tabs-enhanced"
+        "hyper-simple-vibrancy"
     ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
